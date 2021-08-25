f94dd2b6ae48ccb3e7b3d2772a1f1ecb10ddc18c
KGPROJECT <89075931+kgproject1010@users.noreply.github.com>
2021-08-25 12:17:21 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
 from os import system
-system ("git clone https://GitHub.com/kgproject1010/KGuserbot && cd KGuserbot && python3 -m KGuserbit")
+system ("git clone https://GitHub.com/kgproject1010/KGuserbot && cd KGuserbot && python3 -m KGuserbot")
 print ("The KGuserbot has crashed, save the logs and share it in @KGSupportgroup for help.")
